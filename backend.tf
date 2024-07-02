d747df4de74be5ca279cb673c3e461bb51fa270c
SnekhaPugal <snekha.pugal@bootlabstech.com>
2024-07-02 18:45:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket                 = "new-terraform-statefile"
     dynamodb_table         = "terraform-state-lock-dynamo"
-    key                    = "test_tfstat/terraform.tfstate"
+    key                    = "state/terraform.tfstate"
     region                 = "us-west-2"
     
   }
