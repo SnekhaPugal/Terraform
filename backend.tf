cef0baecec6e1fa4ec5e58edc2eddbd38e6318e2
SnekhaPugal <snekha.pugal@bootlabs.com>
2024-07-03 16:45:22 +0530

    updated

--- backend.tf
+++ backend.tf
@@ -2,11 +2,7 @@ terraform {
   backend "s3" {
     bucket                 = "loadbalancer-statefile"
     dynamodb_table         = "terraform-dynamo"
-<<<<<<< HEAD
     key                    = "newfile/terraform.tfstate"
-=======
-    key                    = "state/terraform.tfstate"
->>>>>>> c33b4a0 (updated backend)
     region                 = "eu-north-1"
   }
 }
