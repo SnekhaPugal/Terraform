d50f28245aa5f521c18aac00e61c34c4106081c3
SnekhaPugal <snekha.pugal@bootlabs.com>
2024-07-03 17:50:39 +0530

    update

--- variable.tf
+++ variable.tf
@@ -44,7 +44,7 @@ variable "instance_type" {
 variable "my_key" {
   description = "The name of the existing AWS key pair."
   type        = string
-  default = "key_key"
+  default = "newkey"
 }
 
 
