e7ec085ac5cc36be89c1927585b63e3d4120ce4d
SnekhaPugal <snekha.pugal@bootlabstech.com>
2024-07-03 12:26:01 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -226,7 +226,7 @@ resource "aws_instance" "public_instance" {
   instance_type          = var.instance_type
   subnet_id              = element(aws_subnet.public.*.id, 0)
   vpc_security_group_ids = [aws_security_group.public_sg.id]
-  key_name               = my_key
+  key_name               = var.my_key
 
   tags = {
     Name = "Public Instance"
@@ -239,7 +239,7 @@ resource "aws_instance" "private_instance" {
   instance_type          = var.instance_type
   subnet_id              = element(aws_subnet.private.*.id, 0)
   vpc_security_group_ids = [aws_security_group.private_sg.id]
-  key_name               = my_key
+  key_name               =var.my_key
 
  
 
